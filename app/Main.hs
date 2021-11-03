442f8a1e5892da920a8c85e4ad7398c8a0b448dc
Jorge Jimenez <jorge@jorgejimenez.dev>
2021-11-03 12:49:44 -0700

    work around lmza dependence

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,15 @@ module Main where
 import           ExternalInterfaces.ApplicationAssembly (createApp, loadConfig)
 import           InterfaceAdapters.Config
 import           Network.Wai.Handler.Warp               (run)
-import           SwaggerUI (swagger)
+-- import           SwaggerUI (swagger)
 
 -- | the POSH version of the application: REST service + SwaggerUI
-main :: IO ()
-main = swagger
+-- main :: IO ()
+-- main = swagger
 
 -- | the poor man's version of the application: just a REST service
-simpleMain :: IO ()
-simpleMain = do
+main :: IO ()
+main = do
   config <- loadConfig
   let p = port config
   putStrLn $ "Starting server on port " ++ show p
